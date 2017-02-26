3ca73c9e18d9dd59c8cb4d32154214e2f3675d89
motogeeker <Amy@Amys-MacBook-Air.local>
2017-02-25 17:36:42 -0800

    A lot of garbagge
    Wowowo

--- src/edu/ccsf/amyf/Main.java
+++ src/edu/ccsf/amyf/Main.java
@@ -2,12 +2,13 @@ package edu.ccsf.amyf;
 
 public class Main {
 
-    public static void helper(int i) {
+    ggggg public static void helper(int i) {
         System.out.println("\t\tIn helper! " + i);
     }
 
-    public static void main(String[] args) {
+    bbbbb public static void main(String[] args) {
 	// write your code here
+        ffffuckkkkk
         System.out.println("Hello Amy and Sam.");
         for (int i = 0; i < 10; i++) {
             System.out.println("For loop iteration " + i);
