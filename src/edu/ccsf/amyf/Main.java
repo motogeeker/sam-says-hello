1cd3d19d6a0b693c394e33204453117762c41cb2
motogeeker <Amy@Amys-MacBook-Air.local>
2017-02-25 18:17:29 -0800

    entering 9 works

--- src/edu/ccsf/amyf/Main.java
+++ src/edu/ccsf/amyf/Main.java
@@ -1,18 +1,23 @@
 package edu.ccsf.amyf;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void helper(int i) {
-        System.out.println("\t\tIn helper! " + i);
-    }
-
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello Amy and Sam.");
-        for (int i = 0; i < 10; i++) {
-            System.out.println("For loop iteration " + i);
-            helper(i);
-        }
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("This program will compute your homework average value. How many assignments do you have?");
+            int numAssignments = keyboard.nextInt();
+            if (numAssignments == 0 ) {
+                System.out.println("Since you have 0 homework assignments this program wll not work for you.");
+            }
+            else {
+                System.out.println("What is the value of your first homework assignment?");
+            }
+                for (int assignmentIndex = 0; assignmentIndex< numAssignments; assignmentIndex++) {
+
+                    }
         System.out.println("w00t");
     }
 }
