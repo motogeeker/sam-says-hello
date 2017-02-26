a4b6bece2aedbaab3751ef2d342d93fe4e62bc0c
motogeeker <Amy@Amys-MacBook-Air.local>
2017-02-25 18:46:21 -0800

    works for his numbers

--- src/edu/ccsf/amyf/Main.java
+++ src/edu/ccsf/amyf/Main.java
@@ -6,18 +6,22 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
+        double average = 0;
 
         System.out.println("This program will compute your homework average value. How many assignments do you have?");
-            int numAssignments = keyboard.nextInt();
+        int numAssignments = keyboard.nextInt();
             if (numAssignments == 0 ) {
                 System.out.println("Since you have 0 homework assignments this program wll not work for you.");
             }
             else {
-                System.out.println("What is the value of your first homework assignment?");
+                int runningTotal = 0;
+                for (int assignmentIndex = 0; assignmentIndex < numAssignments; assignmentIndex++) {
+                    System.out.println("Enter a homework assignment value");
+                    int hwValue = keyboard.nextInt();
+                    runningTotal = runningTotal + hwValue;
+                    average = (runningTotal / numAssignments)*10;
+                }
             }
-                for (int assignmentIndex = 0; assignmentIndex< numAssignments; assignmentIndex++) {
-
-                    }
-        System.out.println("w00t");
+        System.out.println("Your homework percentage is %" + average);
     }
 }
